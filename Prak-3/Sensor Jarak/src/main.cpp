cb31ebeb94d6e84310b010f135e3b5342c2dd841
Wildan Nouval Rizki <wildannouvalrizki@gmail.com>
2024-04-02 11:12:27 +0700

    update: upload pengerjaan praktikum-3

---
+++ Prak-3/Sensor Jarak/src/main.cpp
@@ -0,0 +1,36 @@
+#include <Arduino.h>
+
+int trigPin = D1;
+int echoPin = D2;
+int led = D3;
+
+long duration;
+int distance;
+
+void setup() {
+  pinMode(led, OUTPUT);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin,HIGH);
+  distance = 0.034 * duration / 2;
+  Serial.print("Jarak :");
+  Serial.print(distance);
+  Serial.print(" cm");
+
+  if(distance<=10){
+    digitalWrite(led,HIGH);
+  }else{
+    digitalWrite(led,LOW);
+  }
+
+  delay(1000);
+}
